9fd9ed5b976fa0b59976310e0675700d5650f67c
Harsh Singh <harshkushwah2011@gmail.com>
2024-11-10 17:17:06 +0530

    list backend property

---
+++ Behavioural/Iterator/list_backend_property.py
@@ -0,0 +1,42 @@
+class Creature:
+    _strength = 0
+    _agility = 1
+    _intelligence = 2
+
+    def __init__(self) -> None:
+        self.stats = [1, 1, 1]
+
+    @property
+    def strength(self):
+        return self.stats[self._strength]
+
+    @property
+    def agility(self):
+        return self.stats[self._agility]
+
+    @property
+    def intelligence(self):
+        return self.stats[self._intelligence]
+
+    @strength.setter
+    def strength(self, value):
+        self.stats[self._strength] = value
+
+    @agility.setter
+    def agility(self, value):
+        self.stats[self._agility] = value
+
+    @intelligence.setter
+    def intelligence(self, value):
+        self.stats[self._intelligence] = value
+
+    @property
+    def avg_stats(self):
+        return float(sum(self.stats) / len(self.stats))
+
+    @property
+    def num_stats(self):
+        return len(self.stats)
+
+    def __str__(self):
+        return f"S {self.stats[self._strength]}/A  {self.stats[self._agility]}/I {self.stats[self._intelligence]}"
