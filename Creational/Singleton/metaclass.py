1f0b321736d62788661e2bf9247a842ec045cd19
Harsh Singh <harshkushwah2011@gmail.com>
2024-12-23 11:45:31 +0530

    singleton modifying db session in application

--- Creational/Singleton/metaclass.py
+++ Creational/Singleton/metaclass.py
@@ -13,11 +13,30 @@ class Singleton(type):
 
 
 class Database(metaclass=Singleton):
-    def __init__(self):
+    def __init__(self, session: Any = 'dev_session'):
+        self.session = session
         print("Loading database")
+
+    def get_session(self):
+        yield self.session
+
+    def __str__(self):
+        return str(self.session)
 
 
 if __name__ == "__main__":
     d1 = Database()
     d2 = Database()
     print(d1 == d2)
+
+    d2.session = 'test_session'
+
+    print(d1.session, d2.session, Database().session,
+          next(Database().get_session()))
+    print(d1.session == d2.session == Database().session ==
+          next(Database().get_session()))
+
+    get_d1_session = next(d1.get_session())
+    get_d2_session = next(d2.get_session())
+
+    print(get_d1_session, get_d2_session)
